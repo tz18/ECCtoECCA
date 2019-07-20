95d3862cc3d34804d35d009fa3e6ff95af19bdcf
Ramon Rakow <thezeus18@yahoo.ca>
2019-07-20 00:29:27 -0700

    Added substitution by grafting if substituted term has no free variables. Added If but it's incomplete.

--- ECCtoECCA.v
+++ ECCtoECCA.v
@@ -1,5 +1,6 @@
 From Coq Require Import Strings.String.
 From Coq Require Import Strings.Ascii.
+From Coq Require Import Lists.ListSet.
 
 (* Module ECC. *)
 
@@ -21,10 +22,10 @@ Inductive ECCexp: Type :=
   | ePair (e1 e2 A: ECCexp)
   | eFst (e: ECCexp)
   | eSnd (e: ECCexp)
+  | eIf (e e1 e2: ECCexp)
   | eTru
   | eFls
   | eBool
-  | eUndef
 .
 
 Inductive ECCenv: Type :=
@@ -58,11 +59,13 @@ Inductive ECC_LookupR : ECCenv -> string -> ECCexp -> Prop:=
     ECC_LookupR (gTypeDec g' x' a') x A 
   | L_gAssignRest (g': ECCenv) (x x': string) (e e': ECCexp): 
     ECC_LookupR g' x e -> 
-    (x =? x') = false ->
+    (x =? x') = false -> (* should we have this condition? *)
     ECC_LookupR (gAssign g' x' e') x e
 .
 
-(* what if looking up a type and get a value first, or vice versa? *)
+(* Q: what if looking up a type and get a value first, or vice versa? 
+
+*)
 
 
 Example ECC_LookupFirstExample:
@@ -77,39 +80,184 @@ Proof.
   apply L_gTypeDecRest. apply L_gTypeDecFirst. reflexivity.
 Qed.
 
+Print string.
+
+Fixpoint freeVars (e: ECCexp) : (set string) :=
+match e with
+  | eId x => (set_add string_dec x (empty_set string))
+  | eUni U => (empty_set string)
+  | ePi x A B =>  (set_union string_dec (freeVars A) (set_remove string_dec x (freeVars B)))
+  | eAbs x A e => (set_union string_dec (freeVars A) (set_remove string_dec x (freeVars e)))
+  | eApp  e1 e2 => (set_union string_dec (freeVars e1) (freeVars e2))
+  | eLet x e1 e2 => (set_union string_dec (freeVars e1) (freeVars e2))
+  | eSig x A B => (set_union string_dec (freeVars A) (set_remove string_dec x (freeVars B)))
+  | ePair e1 e2 A => (set_union string_dec (set_union string_dec (freeVars e1) (freeVars e2)) (freeVars A))
+  | eFst e => (freeVars e)
+  | eSnd e => (freeVars e)
+  | eIf e e1 e2 => (set_union string_dec (set_union string_dec (freeVars e) (freeVars e1)) (freeVars e2))
+  | eTru => (empty_set string)
+  | eFls => (empty_set string)
+  | eBool => (empty_set string)
+end.
+
+Compute (freeVars (eId "x")).
+Compute (freeVars (eApp (eId "x") (eId "x"))).
+Compute (freeVars (eApp (eId "x") (eId "y"))).
+Compute (freeVars (eAbs "x" (eId "a") (eId "b"))).
+Compute (freeVars (eAbs "x" (eId "x") (eId "b"))).
+Compute (freeVars (eAbs "x" (eId "a") (eId "x"))).
+Compute (freeVars (eAbs "x" (eId "x") (eId "x"))).
+
+Fixpoint graft (x: string) (arg body: ECCexp) :=
+match body with
+  | eId x' => if (x =? x') then arg else body
+  | eAbs x' A e =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) e) 
+        else (eAbs x' (graft x arg A) (graft x arg e))
+  | ePi x' A B => 
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) B) 
+        else (eAbs x' (graft x arg A) (graft x arg B))
+  | eLet x' e1 e2 =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg e1) e2) 
+        else (eAbs x' (graft x arg e1) (graft x arg e2))
+  | eSig x' A B =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) B) 
+        else (eAbs x' (graft x arg A) (graft x arg B))
+  | eApp e1 e2 => (eApp (graft x arg e1) (graft x arg e2))
+  | eUni U => body
+  | ePair e1 e2 A => (ePair (graft x arg e1) (graft x arg e2) (graft x arg A))
+  | eFst e => (eFst (graft x arg e))
+  | eSnd e => (eSnd (graft x arg e))
+  | eIf e e1 e2 => (eIf (graft x arg e) (graft x arg e1) (graft x arg e2))
+  | eTru => eTru
+  | eFls => eFls
+  | eBool => eBool
+end.
+
+(*Fixpoint trickySubst (x: string) (arg body: ECCexp) (freeVarsInArg: (set string)) :=
+match body with
+  | eId x' => if (x =? x') then arg else body
+  | eAbs x' A e =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) e) 
+        else (eAbs x' (graft x arg A) (graft x arg e))
+  | ePi x' A B => 
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) B) 
+        else (eAbs x' (graft x arg A) (graft x arg B))
+  | eLet x' e1 e2 =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg e1) e2) 
+        else (eAbs x' (graft x arg e1) (graft x arg e2))
+  | eSig x' A B =>
+      if (x =? x') 
+        then (eAbs x' (graft x arg A) B) 
+        else (eAbs x' (graft x arg A) (graft x arg B))
+  | eApp e1 e2 => (eApp (graft x arg e1) (graft x arg e2))
+  | eUni U => body
+  | ePair e1 e2 A => (ePair (graft x arg e1) (graft x arg e2) (graft x arg A))
+  | eFst e => (eFst (graft x arg e))
+  | eSnd e => (eSnd (graft x arg e))
+  | eIf e e1 e2 => (eIf (graft x arg e) (graft x arg e1) (graft x arg e2))
+  | eTru => eTru
+  | eFls => eFls
+  | eBool => eBool
+end.*)
+
+Definition subst (x: string) (arg body: ECCexp) :=
+match (freeVars arg) with
+  | nil => (graft x arg body)
+  | frees => (eId "NOT IMPLEMENTED") (*(trickysubst x arg body frees)*)
+end.
+
+
 (* -Step- *)
 
-Inductive ECC_RedR : ECCenv -> ECCexp -> ECCenv -> ECCexp -> Prop :=
+Inductive ECC_RedR : ECCenv -> ECCexp -> ECCexp -> Prop :=
   | R_ID (g: ECCenv) (x: string) (e': ECCexp) :
-    ECC_LookupR g x e' -> ECC_RedR g (eId x) g e'
+    ECC_LookupR g x e' -> ECC_RedR g (eId x) e'
   | R_App (g: ECCenv) (x: string) (A body arg: ECCexp) :
-    ECC_RedR g (eApp (eAbs x A body) arg) (gAssign g x arg) arg
+    ECC_RedR g (eApp (eAbs x A body) arg) (subst x arg body) (*do anything with env here?*)
   | R_Fst (g: ECCenv) (e1 e2 A: ECCexp) : 
-    ECC_RedR g (eFst (ePair e1 e2 A)) g e1
+    ECC_RedR g (eFst (ePair e1 e2 A)) e1
   | R_Snd (g: ECCenv) (e1 e2 A: ECCexp) : 
-    ECC_RedR g (eSnd (ePair e1 e2 A)) g e2
+    ECC_RedR g (eSnd (ePair e1 e2 A)) e2
   | R_Let (g: ECCenv) (x: string) (e1 e2: ECCexp) :
-    ECC_RedR g (eLet x e1 e2) (gAssign g x e1) e2
+    ECC_RedR g (eLet x e1 e2) (subst x e1 e2)  (*or here?*)
+  | R_IfTru (g: ECCenv) (e1 e2: ECCexp) :
+    ECC_RedR g (eIf eTru e1 e2) e1
+  | R_IfFls (g: ECCenv) (e1 e2: ECCexp) :
+    ECC_RedR g (eIf eFls e1 e2) e2
 .
 
-Inductive ECC_RedClosR : ECCenv -> ECCexp -> ECCenv -> ECCexp -> Prop :=
-  | R_RedR (g g': ECCenv) (e e': ECCexp):
+Inductive ECC_RedClosR : ECCenv -> ECCexp -> ECCexp -> Prop :=
+  (*| R_RedR (g g': ECCenv) (e e': ECCexp): (* maybe don't need this one? it follows from refl + trans*)
       ECC_RedR g e g' e' ->
-      ECC_RedClosR g e g' e'
+      ECC_RedClosR g e g' e'*)
   | R_Refl (g: ECCenv) (e: ECCexp):
-      ECC_RedClosR g e g e
-  | R_Trans (g g' g'': ECCenv) (e e' e'': ECCexp) :
-      ECC_RedR g e g' e' -> (* should be clos?*)
-      ECC_RedClosR g' e' g'' e'' ->
-      ECC_RedClosR g e g'' e''
-  | R_CongLet (g g': ECCenv) (e e1 e2: ECCexp) (x: string) : 
-      ECC_RedClosR (gAssign g x e) e1 g' e2 -> (* exists g' ?*) 
-      ECC_RedClosR g (eLet x e e1) g (eLet x e e2) (* should be just e2? *)
-  | R_CongLam1 (g g': ECCenv) (A A' M: ECCexp) (x: string) :
-      ECC_RedClosR g A g' A' -> (* should be g?*)
-      ECC_RedClosR g (eAbs x A M) g (eAbs x A' M)
-(*...*)
+      ECC_RedClosR g e e
+  | R_Trans (g: ECCenv) (e e' e'': ECCexp) :
+      ECC_RedR g e e' ->
+      ECC_RedClosR g e' e'' ->
+      ECC_RedClosR g e e''
+  | R_CongLet (g: ECCenv) (e e1 e2: ECCexp) (x: string) : 
+      ECC_RedClosR (gAssign g x e) e1 e2 ->  
+      ECC_RedClosR g (eLet x e e1) (eLet x e e2)
+  | R_CongLam1 (g: ECCenv) (A A' e e': ECCexp) (x: string) :
+      ECC_RedClosR g A A' ->
+      ECC_RedClosR (gTypeDec g x A) e e' ->
+      ECC_RedClosR g (eAbs x A e) (eAbs x A' e')
+  | R_CongPi (g: ECCenv) (A A' B B': ECCexp) (x: string) :
+      ECC_RedClosR g A A' ->
+      ECC_RedClosR (gTypeDec g x A) B B' ->
+      ECC_RedClosR g (ePi x A B) (ePi x A' B')
+  | R_CongSig (g: ECCenv) (A A' B B': ECCexp) (x: string) :
+      ECC_RedClosR g A A' ->
+      ECC_RedClosR (gTypeDec g x A) B B' ->
+      ECC_RedClosR g (eSig x A B) (eSig x A' B')
+  | R_CongPair (g: ECCenv) (e1 e1' e2 e2' A A': ECCexp) : 
+      ECC_RedClosR g e1 e1' ->
+      ECC_RedClosR g e2 e2' ->
+      ECC_RedClosR g A A'   ->
+      ECC_RedClosR g (ePair e1 e2 A) (ePair e1' e2' A')
+  | R_CongApp (g: ECCenv) (e1 e1' e2 e2': ECCexp) :
+      ECC_RedClosR g e1 e1' ->
+      ECC_RedClosR g e2 e2' ->
+      ECC_RedClosR g (eApp e1 e2) (eApp e1' e2')
+  | R_CongFst (g: ECCenv) (V V': ECCexp) :
+      ECC_RedClosR g V V' ->
+      ECC_RedClosR g (eFst V) (eFst V')
+  | R_CongSnd (g: ECCenv) (V V': ECCexp) :
+      ECC_RedClosR g V V' ->
+      ECC_RedClosR g (eSnd V) (eSnd V')
+  | R_CongIf (g: ECCenv) (e e' e1 e1' e2 e2': ECCexp) :
+      ECC_RedClosR g e e' ->
+      ECC_RedClosR g e1 e1' ->
+      ECC_RedClosR g e2 e2' ->
+      ECC_RedClosR g (eIf e e1 e2) (eIf e' e1' e2')
 .
+
+Inductive ECC_CongConv: ECCenv -> ECCexp -> ECCexp -> Prop :=
+  | C_Cong (g: ECCenv) (e e1 e2: ECCexp) :
+      ECC_RedClosR g e1 e ->
+      ECC_RedClosR g e2 e ->
+      ECC_CongConv g e1 e2
+  | C_CongIta1 (g: ECCenv) (e1 A e e2 e2': ECCexp) (x: string) :
+      ECC_RedClosR g e1 (eAbs x A e) ->
+      ECC_RedClosR g e2 e2' ->
+      ECC_CongConv (gTypeDec g x A) e (eApp e2' (eId x)) ->
+      ECC_CongConv g e1 e2
+  | C_CongIta2 (g: ECCenv) (e e1 e1' e2 A : ECCexp) (x: string) :
+      ECC_RedClosR g e1 e1' ->
+      ECC_RedClosR g e2 (eAbs x A e) ->
+      ECC_CongConv (gTypeDec g x A) (eApp e1' (eId x)) e ->
+      ECC_CongConv g e1 e2
+.
+
 
 (* ECC Notation *)
 
@@ -264,7 +412,6 @@ Fixpoint trans  (ns: nat) (e: ECCexp) (K: ECCAcont) : ECCAconf :=
     | eTru => (fill_hole (acpVal avTru) K)
     | eFls => (fill_hole (acpVal avFls) K)
     | eBool=> (fill_hole (acpVal avBool) K)
-    | eUndef => (fill_hole (acpVal avUndef) K)
     | eUni u => (fill_hole (acpVal (avUni u)) K)
 end.
 
