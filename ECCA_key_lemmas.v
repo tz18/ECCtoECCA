da336adaf9c0238d3731c73ed6e8b0f1c48096c9
Paulette Koronkevich <pkoronkevich3@gmail.com>
2020-06-04 15:15:07 -0700

    actually finishing cut lemmas

--- ECCA_key_lemmas.v
+++ ECCA_key_lemmas.v
@@ -2,181 +2,48 @@ Require Export ECCA_typing.
 Require Export ECCA_equiv_lemmas.
 Require Export ECCA_continuations.
 
-Check wk.
-
 Lemma Cut (g: ECCAenv) (K : cont) (N: ECCAexp) (A B: ECCAexp):
 (ECCA_cont_has_type g K (Cont N A B) ->
 ECCA_has_type g N A ->
-ECCA_has_type g (fill_hole N K) B )%ECCA.
+ECCA_has_type g (fill_hole N K) B)%ECCA.
 Proof. 
 intros. inversion H ; subst ; cbv.
 - assumption. 
 - cut (@bind N 0 (@wk 0 B) = B); simpl_term_eq. 
-  + intros. rewrite <- H1. eapply aT_Let with (n:= N) (m:= M) (A:=A) (B:=(wk B)) (x:=y) (g:=g).
-    assumption.
+  intros. rewrite <- H1. eapply aT_Let with (n:= N) (m:= M) (A:=A) (B:=(wk B)) (x:=y) (g:=g); assumption.
 Qed.
 
-Lemma equivalence_is_equivalence (g: ECCAenv) (y: atom) (N N' M: ECCAexp) (A B: ECCAexp):
-((g |- N : A) ->
-(g |- N' : A) ->
-ECCA_Equiv g N N' ->
-
-(((g, y = N), y : A) |- M : B) ->
-(((g, y = N'), y : A) |- M : B))%ECCA.
-Proof. (* intros. induction H2; default_simp.
-1: {shelve. } (* need to show that lookuptype is at most one to one *)
-6: {eapply aT_Let. 
-    - apply IHECCA_has_type1. 
-    - } *)
-Abort.
-
-(* Admitting for now. Need a nicer way to formulate well formedness
-and/or the idea that a certain id does not already exist in the g *)
-
-Fixpoint domain_of_env (g: ECCAenv): atoms :=
-match g with
-| Empty => empty 
-| Assum g x A => (add x (domain_of_env g))
-| Def g x A => (add x (domain_of_env g))
-| Eq g e1 e2 => domain_of_env g
-end.
-
-Lemma weakening (g : ECCAenv) (x : atom) (N A A' : ECCAexp):
+Lemma weakening (g : ECCAenv) (x : name) (N A A' U : ECCAexp):
   ECCA_has_type g N A ->
-  (* x `notin` (domain_of_env g) -> *)
-  ECCA_has_type (Assum g x A') N A.
+  ECCA_has_type (ctx_cons g x (Assum A')) N A.
+Proof.
 Admitted.
 
-Lemma def_weakening (g : ECCAenv) (x : atom) (N N' A A' : ECCAexp):
+Lemma def_weakening (g : ECCAenv) (x : name) (N N' A A' : ECCAexp):
   ECCA_has_type g N A ->
-  (* x `notin` (domain_of_env g) -> *)
-  ECCA_has_type (Assum (Def g x N') x A') N A.
+  ECCA_has_type (ctx_cons g x (Def N' A')) N A.
 Admitted.
-Require Import Coq.Program.Equality.
 
-
-(* Dependent induction works for the first case, but not for the other cases. 
-Normal induction works for the other cases, but not the first case *)
-
-Lemma equivalence_in_derivation1 (g: ECCAenv) (N N' M : ECCAexp) (A B : ECCAexp) (x : atom):
+Lemma equivalence_in_derivation (g: ECCAenv) (N N' M : ECCAexp) (A B : ECCAexp) (x : name):
 ECCA_has_type g N A ->
 ECCA_has_type g N' A ->
 ECCA_Equiv g N N' ->
-ECCA_has_type (Assum (Def g x N) x A) M B ->
-ECCA_has_type (Assum (Def g x N') x A) M B.
-Proof.
-  intros. induction H2; auto.
-  -  shelve. (* apply aT_Var. inversion H2. 
-    + auto. shelve.
-    + default_simp.  *)
- (* where is g0 coming from??? *)
-  - apply aT_Sig.
-    + assumption.
-    + apply weakening. assumption.
-(*   - shelve. (* apply aT_Prod_Prop with (i := i).
-    + assumption.
-    + apply weakening. assumption. *)
-  - shelve. (*  apply aT_Prod_Type with (i := i).
-    + assumption.
-    + apply weakening. assumption. *)
-  - apply aT_Lam. apply weakening. assumption.
-  - apply aT_Let with (A := A0).
-    + assumption.
-    + apply def_weakening. assumption.
-  - apply aT_Conv with (A := A0) (U := U).
-    + assumption.
-    + assumption.
-    + shelve. (* again, where is g0 coming from??? *)
-  - apply aT_App with (A':= A'); assumption.
-  - apply aT_Fst with (B := B) (x := x0). assumption.
-  - apply aT_Snd with (A := A0). *)
+ECCA_has_type (ctx_cons g x (Def N A)) M B ->
+ECCA_has_type (ctx_cons g x (Def N' A)) M B.
 Admitted.
 
-Lemma equivalence_in_derivation2 (g: ECCAenv) (N N' M : ECCAexp) (A B : ECCAexp) (x : atom):
-ECCA_has_type g N A ->
-ECCA_has_type g N' A ->
-ECCA_Equiv g N N' ->
-ECCA_has_type (Assum (Def g x N) x A) M B ->
-ECCA_has_type (Assum (Def g x N') x A) M B.
-Proof.
-  intros. dependent induction H2; auto.
-  - apply aT_Var. inversion H2.
-    + auto.
-    + default_simp. 
- (* where is g0 coming from??? *)
-  - apply aT_Sig.
-    + apply IHECCA_has_type1 with (N0:=N); auto.
-    + apply weakening. apply IHECCA_has_type2 with (N0:=N'); auto. shelve.
-(*   - shelve. (* apply aT_Prod_Prop with (i := i).
-    + assumption.
-    + apply weakening. assumption. *)
-  - shelve. (*  apply aT_Prod_Type with (i := i).
-    + assumption.
-    + apply weakening. assumption. *)
-  - apply aT_Lam. apply weakening. assumption.
-  - apply aT_Let with (A := A0).
-    + assumption.
-    + apply def_weakening. assumption.
-  - apply aT_Conv with (A := A0) (U := U).
-    + assumption.
-    + assumption.
-    + shelve. (* again, where is g0 coming from??? *)
-  - apply aT_App with (A':= A'); assumption.
-  - apply aT_Fst with (B := B) (x := x0). assumption.
-  - apply aT_Snd with (A := A0). *)
-Admitted.
-     
   
-Lemma Cut_modulo_equivalence (g: ECCAenv) (K : cont) (N N': ECCAexp) (A B B': ECCAexp):
+Lemma Cut_modulo_equivalence (g: ECCAenv) (K : cont) (N N': ECCAexp) (A B: ECCAexp):
 (ECCA_cont_has_type g K (Cont N A B) ->
 ECCA_has_type g N A ->
 ECCA_has_type g N' A ->
 ECCA_Equiv g N N' ->
-exists B', ECCA_has_type g (fill_hole N' K) B' /\ (B' =a= B))%ECCA.
+ECCA_has_type g (fill_hole N' K) B)%ECCA.
 Proof. 
 intros. inversion H ; subst ; cbv.
-- exists B. split.
-  + eauto.
-  + eauto.
-- exists (subst y N' B). split.
-  + eapply aT_Let with (n:= N') (m:= M) (A:=A) (B:=B) (x:=y) (g:=g).
-    * assumption.
-    * apply equivalence_in_derivation1 with (N := N); assumption.
-  + apply subst_no_fv_aeq. auto.
+- assumption. 
+- cut (@bind N' 0 (@wk 0 B) = B); simpl_term_eq.
+  intros. rewrite <- H3. eapply aT_Let with (n:= N') (m:= M) (A:=A) (B:=(wk B)) (x:=y) (g:=g).
+  * assumption.
+  * apply equivalence_in_derivation with (N := N); assumption.
 Qed. 
-
-
-
-(* | aT_Conv (g: ECCAenv) (e A B U: ECCAexp) :
-  (g |- e : A) ->
-  (g |- B : U) ->
-  (ECCA_sub_type g A B) ->
-  (g |- e : B)
-       
-Unable to unify "(g |- eLet y N M : subst y N B)%ECCA" with "(g |- eLet y N M : B)%ECCA".
- *)
-
-
-(* 
-Goal ECCA_has_type Empty (Fst (Pair Tru Fls 
-    (Sig X Bool 
-          (If X Bool (Pi Y Bool Bool))))) Bool.
-Proof.
-eapply aT_Fst with (A:= Bool). eapply aT_Pair.
-  - apply aT_True.
-  - cbv. apply aT_Conv with (A := Bool) (U := (Uni (uType 0))).
-    + apply aT_False.
-    + eapply aT_Conv.
-      * apply aT_If with (e:= Tru) (e1:= Bool) (e2:= (P 1 : Bool -> Bool)%ECCA) (x:= 20) (B:= (Uni (uType 0))) (U:= Uni (uType 1)). 
-        -- apply aT_Ax_Type.
-        -- auto.
-        -- auto.
-        -- cbv. eapply aT_Prod_Type.
-          ++ auto.
-          ++ auto.
-      * apply aT_Ax_Type.
-      * cbn. apply aST_Cong. apply aE_EquivAlpha. apply aeq_id.
-    + apply aST_Cong. apply aE_Equiv with (e:= eBool).
-      * auto.
-      * eauto.
-Qed. *)
