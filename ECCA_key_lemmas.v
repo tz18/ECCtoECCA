d092af5701b9a746fe099c8345f2c6a5ee59e491
Paulette Koronkevich <pkoronkevich3@gmail.com>
2020-08-23 08:58:33 -0700

    some attemps at het cut

--- ECCA_key_lemmas.v
+++ ECCA_key_lemmas.v
@@ -51,7 +51,16 @@ Proof.
   - exists (eUni (uType 0)). apply aT_Bool. auto.
   - exists (eUni (uType 0)). apply aT_Bool. auto.
   - apply IHECCA_has_type1.
-  - exists (eUni (uType 0)). eapply aT_Sig.
+  - exists (eUni (uType 0)). eapply aT_Sig. shelve. shelve.
+  - exists (eUni (uType 0)). apply aT_Ax_Prop. shelve.
+  - exists (eUni (uType (S i))). apply aT_Ax_Type. shelve.
+  - exists (eUni (uProp)). eapply aT_Prod_Prop. shelve. shelve.
+  - shelve.
+  - shelve.
+  - exists U. auto.
+  - shelve.
+  - shelve.
+  - shelve. 
 Admitted. 
 
 Lemma has_type_wf_g (g: ECCAenv) (N: ECCAexp) (A: ECCAexp) (x : name):
@@ -124,8 +133,74 @@ Qed.
 Lemma appendEnv_rearrange {V:nat}(g g': @ECCAenv V) (x: name) (a: ctxmem) :
   ((appendEnv g g')& x ~ a) = (appendEnv (g & x ~ a) g').
 Proof.
-  induction g; simpl; auto.
+  induction g; auto.
 Qed.
+
+Lemma cont_return_type_well_typed (g : ECCAenv) (K : cont_r) (M : ECCAconf) (A B: ECCAexp):
+  ECCA_cont_has_type g (unrestrict_cont K) (Cont M A B) ->
+  exists U , ECCA_has_type g B U.
+Proof.
+  intros. dependent induction H.
+  + apply type_well_typed in H. auto. 
+  + apply type_well_typed in H0.
+    (* need B : U but have (open y (wk B)) : U *)
+    shelve.
+Admitted.
+
+Lemma shift_no_free (g : ECCAenv) (B: ECCAexp) (x: name):
+  (exists U, ECCA_has_type g B U) ->
+  B = (open x (wk B)).
+Admitted.
+
+Lemma aT_Let_no_bind (g : ECCAenv) (M : @ECCAexp 1) (N A B: ECCAexp) (x: name):
+  ECCA_has_type g N A ->
+  ECCA_has_type (g & x ~ Def N A) (open x M) B ->
+  (exists U, ECCA_has_type g B U) ->
+  ECCA_has_type g (eLet N M) B.
+Proof.
+  intros. cut ((bind N (wk B)) = B).
+  - intros. rewrite <- H2. apply aT_Let with (A:=A) (x:=x).
+    * apply H.
+    * apply shift_no_free with (x:=x) in H1.
+      rewrite <- H1. auto.
+  - simpl_term_eq.
+Qed. 
+
+Lemma aT_If_no_bind (g : ECCAenv) (V B M1 M2: ECCAexp) (x y: name):
+  ECCA_has_type g V eBool ->
+  ECCA_has_type (g & y ~ (Eq V eTru)) M1 B ->
+  ECCA_has_type (g & y ~ (Eq V eFls)) M2 B ->
+  (exists U, ECCA_has_type (g & x ~ Assum eBool) B U) ->
+  ECCA_has_type g (eIf V M1 M2) B.
+Proof.
+  intros. cut ((bind V (wk B)) = B).
+  - intros. rewrite <- H3. assert (H4:=H2). destruct H2. apply aT_If with (x:=x) (y:=y) (U:=x0).
+    * apply shift_no_free with (x:=x) in H4 as H5.
+      rewrite H5 in H2. auto.
+    * auto.
+    * cut ((bind eTru (wk B)) = B). intros. 
+      rewrite H5. auto. simpl_term_eq.
+    * cut ((bind eFls (wk B)) = B). intros. 
+      rewrite H5. auto. simpl_term_eq.
+  - simpl_term_eq.
+Qed. 
+
+Lemma open_wk_het_comp (K : cont_r) (M : ECCAconf) (x: name):
+  open x (het_compose_r (wk_cont K) M) = (het_compose_r K (@open_conf 0 x M)).
+Admitted.
+
+Lemma defn_well_typed (g: ECCAenv) (N M B: ECCAexp) :
+  ECCA_has_type g (eLet N M) B ->
+  exists A, ECCA_has_type g N A.
+Proof.
+  intros. dependent induction H.
+  - exists A. auto.
+  - apply IHECCA_has_type1 with (M0:=M). auto.
+Qed. 
+  
+                
+
+Set Printing Implicit.
 
 Lemma Hetereogeneous_Cut (g g': ECCAenv) (K : cont_r) (M M': ECCAconf) (A B: ECCAexp):
 ECCA_cont_has_type (appendEnv g g') (unrestrict_cont K) (Cont M' A B) ->
@@ -137,12 +212,21 @@ ECCA_has_type (appendEnv g g') (het_compose_r K M) B.
   - erewrite het_compose_equal_fill_hole. apply Cut_modulo_equivalence with (A := A) (N:=M').
     assumption. inversion H. assumption. assumption. apply append_env_weakening with (g':=g') in H0.
     assumption. apply aE_Symm. apply append_env_equiv_weakening with (g':=g') in H2. assumption.
-  - simpl.
-    (* g & x ~ Def n A0 |- m : B -> g |- eLet n m : B *)
-    (* ... K is well-typed at g |- (Cont M' A B), g |- B : Type*) inversion H. inversion H0. rewrite <- H8. rewrite <- H13. apply aT_Let with (A:=A2) (x:=x0).
-    + apply append_env_weakening with (g':=g') in H12. assumption.
-    + erewrite appendEnv_rearrange. admit.
+  - simpl. pose (defn_well_typed g n m A H0)as H3. destruct H3.
+    eapply aT_Let_no_bind.
+    * apply append_env_weakening with (g':=g') in H3. apply H3.
+    * pose (appendEnv_rearrange g g' x (Def n A)) as H4.
+      pose (open_wk_het_comp K m x) as H5. rewrite H5. shelve.
+    * apply cont_return_type_well_typed in H. auto.
+  - simpl. apply aT_If_no_bind with (x:=x) (y:=y).
+    * shelve.
+    * pose (appendEnv_rearrange g g' y (Eq (flattenECCAval v) eTru)) as H3.
+      shelve. (*??? what the heLL*)
+    * shelve.
+    * apply cont_return_type_well_typed in H.
+      destruct H. apply weakening with (x:=x) (A':=eBool) in H.
+      exists x0. auto.
       
-Admitted.
+Admitte.
 
  
